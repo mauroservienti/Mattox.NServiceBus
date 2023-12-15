e683585b1673a0df971ede75dbe463d13d1a5750
Mauro Servienti <mauro.servienti@gmail.com>
2023-12-15 06:40:26 +0100

    Rename everything once more

--- src/Mattox.NServiceBus/LearningEndpoint.cs
+++ src/Mattox.NServiceBus/LearningEndpoint.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.Configuration;
 
-namespace Maddox.NServiceBus;
+namespace Mattox.NServiceBus;
 
 public class LearningEndpoint : NServiceBusEndpoint<LearningTransport>
 {
