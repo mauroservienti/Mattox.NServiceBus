d32ccc9ac759d42897b744b05907eafecdc42539
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-01 12:03:20 +0100

    Support for custom recoverability policy

--- src/Mattox.NServiceBus/EndpointRecoverability.cs
+++ src/Mattox.NServiceBus/EndpointRecoverability.cs
@@ -1,10 +1,13 @@
+using NServiceBus.Transport;
+
 namespace Mattox.NServiceBus;
 
 public class EndpointRecoverability
 {
     internal Func<CancellationToken,Task>? OnRateLimitStartedCallback { get; private set; }
     internal Func<CancellationToken,Task>? OnRateLimitEndedCallback { get; private set; }
-    internal Action<RetryFailedSettings> OnFailedMessageCallback { get; private set; }
+    internal Action<RetryFailedSettings>? OnFailedMessageCallback { get; private set; }
+    internal Func<RecoverabilityConfig, ErrorContext, RecoverabilityAction>? CustomRecoverabilityPolicy { get; private set; }
 
     public void OnRateLimitStarted(Func<CancellationToken, Task> onRateLimitStarted)
     {
@@ -19,5 +22,10 @@ public class EndpointRecoverability
     public void OnFailedMessage(Action<RetryFailedSettings> retrySettings)
     {
         OnFailedMessageCallback = retrySettings;
+    }
+
+    public void UseCustomRecoverabilityPolicy(Func<RecoverabilityConfig, ErrorContext, RecoverabilityAction> policy)
+    {
+        CustomRecoverabilityPolicy = policy;
     }
 }
